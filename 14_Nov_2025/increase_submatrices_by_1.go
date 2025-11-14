a3fc77c67f0574ffdf9db96ece2c19d209e1bd5b
Sagar Sen <sagarsen27842@gmail.com>
2025-11-15 02:14:26 +0530

    Leetcode 2536: 14 Nov 2025: increase submatrices by 1 - with difference array technique

--- 14_Nov_2025/increase_submatrices_by_1.go
+++ 14_Nov_2025/increase_submatrices_by_1.go
@@ -23,6 +23,37 @@ func rangeAddQueries(n int, queries [][]int) [][]int {
 	return mat
 }
 
+// DIFFERENCE ARRAY APPROACH
+func rangeAddQueries2(n int, queries [][]int) [][]int {
+	mat := make([][]int, n)
+
+	for i := range n {
+		mat[i] = make([]int, n)
+	}
+
+	for _, query := range queries {
+		r1 := query[0]
+		c1 := query[1]
+		r2 := query[2]
+		c2 := query[3]
+
+		for row := r1; row <= r2; row++ {
+			mat[row][c1]++
+			if c2+1 < n {
+				mat[row][c2+1]--
+			}
+		}
+
+	}
+	for row := range n {
+		for col := 1; col < n; col++ {
+			mat[row][col] += mat[row][col-1]
+		}
+	}
+	return mat
+}
+
 func main() {
 	fmt.Println(rangeAddQueries(3, [][]int{{1, 1, 2, 2}, {0, 0, 1, 1}}))
+	fmt.Println(rangeAddQueries2(3, [][]int{{1, 1, 2, 2}, {0, 0, 1, 1}}))
 }
